5d7b08095bc6c92e14b08d0f3b45f77896f2047a
nvllvs <nvllvs@gmail.com>
2016-11-18 20:02:24 +0100

    first commit

---
+++ demoty.py
@@ -0,0 +1,51 @@
+import PIL, os
+from PIL import ImageEnhance, Image, ImageOps, ImageDraw, ImageFont
+from random import randint
+
+TEMPLATE_FILENAME = 'template.jpg'
+TEMPLATE_COORDS = (75, 45, 499, 373)
+PHRASES_FILENAME = 'phrases.txt'
+
+UPPER_FONT = 'times.ttf'
+LOWER_FONT = 'arial.ttf'
+
+W = 574
+H = 522
+
+def getRandomImage():
+    fileList = []
+    for dirpath, dirnames, filenames in os.walk('.'):
+        for filename in [f for f in filenames if f.endswith('.jpg') or f.endswith('.png')]:
+            fileList.append(filename)
+    return fileList[randint(0, len(fileList) - 1)]
+
+def getPhrases():
+    with open(PHRASES_FILENAME) as file:
+        content = file.read().splitlines()
+    return content
+
+def getRandomPhrase():
+    phraseList = getPhrases()
+    return phraseList[randint(0, len(phraseList) - 1)]
+
+def makeImage():
+    frame = PIL.Image.open(TEMPLATE_FILENAME)
+    demot = PIL.Image.open(getRandomImage())
+    demot = demot.resize((TEMPLATE_COORDS[2] - TEMPLATE_COORDS[0], TEMPLATE_COORDS[3] - TEMPLATE_COORDS[1]), PIL.Image.ANTIALIAS)
+    frame.paste(demot, TEMPLATE_COORDS)
+
+    draw = ImageDraw.Draw(frame)
+
+    upperText = getRandomPhrase()
+    upperTextFont = ImageFont.truetype(UPPER_FONT, 45)
+    w, h = upperTextFont.getsize(upperText)
+    draw.text((int((W-w)/2),390), upperText, font=upperTextFont)
+
+    lowerText = getRandomPhrase()
+    lowerTextFont = ImageFont.truetype(LOWER_FONT, 16)
+    w, h = lowerTextFont.getsize(lowerText)
+    draw.text((int((W-w)/2),460), lowerText, font=lowerTextFont)
+    
+    frame.show()
+
+makeImage()
